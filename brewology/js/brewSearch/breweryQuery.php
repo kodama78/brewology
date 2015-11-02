d3358bcbb3524191c139ecc60a4dae966f91e40c
Shawn Otomo <kodama78@gmail.com>
2015-11-01 16:50:33 -0800

    found MySQL statement for finding breweries by zip or city

--- brewology/js/brewSearch/breweryQuery.php
+++ brewology/js/brewSearch/breweryQuery.php
@@ -9,16 +9,30 @@ $conn = mysqli_connect('localhost', 'root', '', 'compiled_breweries');
 
 $location = $_POST['location'];
 
-$find_breweries = "SELECT * FROM `california_breweries` WHERE zip1 = '$location'";
-$result = mysqli_query($conn, $find_breweries);
+$find_brewery = "SELECT `latitude`,`longitude` FROM `california_breweries` WHERE zip1 = '$location'";
+$result = mysqli_query($conn, $find_brewery);
 mysqli_error($conn);
+
 
 if (mysqli_num_rows($result) > 0) {
     while ($inner_result = mysqli_fetch_assoc($result)) {
-        $breweries[] = $inner_result;
+        $brewery = $inner_result;
     }
-    echo json_encode($breweries);
+    print_r($breweries);
 }
 else{
     echo 'No breweries in that area code';
 }
+
+
+//SELECT STATEMENT FOR BREWERIES BY DISTANCE QUERY
+//SELECT
+//`id`,
+//`name`,
+//ACOS( SIN( RADIANS( `latitude` ) ) * SIN( RADIANS($lat ) ) + COS( RADIANS( `latitude` ) )
+//    * COS( RADIANS( $lat )) * COS( RADIANS( `longitude` ) - RADIANS( $long )) ) * 3959 AS `distance`
+//FROM `california_breweries`
+//WHERE
+//ACOS( SIN( RADIANS( `latitude` ) ) * SIN( RADIANS( $lat ) ) + COS( RADIANS( `latitude` ) )
+//    * COS( RADIANS( $lat )) * COS( RADIANS( `longitude` ) - RADIANS( $long )) ) * 3959 < 100
+//ORDER BY `distance`;
